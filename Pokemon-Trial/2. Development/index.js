7c9babac378602436458d38b31fa0545adb58e3b
XiDekatria <junniellev@gmail.com>
2022-07-03 14:50:12 +0800

    Pokemon Clone
    Tools: HTML Canvas, Vanilla JavaScript, Tiled Software
    
    - Completed the Map Layout
    - Completed the Player

---
+++ Pokemon-Trial/2. Development/index.js
@@ -0,0 +1,299 @@
+// 1. Setting up the canvas
+// selects an html element into the js file
+const canvas = document.querySelector('canvas');
+
+// reference canvas context (2d API?)
+const context = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 70) {
+    collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+const boundaries = []
+
+const offset = {
+    x: -1550,
+    y: -700
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1025)
+            boundaries.push (
+                new Boundary({
+                    position: {
+                        x: j * Boundary.width + offset.x,
+                        y: i * Boundary.height + offset.y
+                    }
+                })
+            )
+    })
+})
+
+// context.fillStyle = 'white'
+// context.fillRect (0, 0, canvas.width, canvas.height)
+
+// 2. Inserting the map and the character
+// insert image html within js
+const image = new Image ()
+image.src = './Images/Pokemon Style Game Map Layout.png'
+
+const foregroundImage = new Image ()
+foregroundImage.src = './Images/foregroundObjects.png'
+
+const playerDownImage = new Image ()
+playerDownImage.src = './Images/playerDown.png'
+
+const playerUpImage = new Image ()
+playerUpImage.src = './Images/playerUp.png'
+
+const playerLeftImage = new Image ()
+playerLeftImage.src = './Images/playerLeft.png'
+
+const playerRightImage = new Image ()
+playerRightImage.src = './Images/playerRight.png'
+
+// calls the image when it is loaded + position (x, y) -- (bg image first then player image)
+// cropping character
+// 1-4 cropping (crop start x, y, width, height)
+// 5-8 actual (position x, y, width, height)
+// image.onload = () => {
+//     context.drawImage (image, -1550, -650)
+//     context.drawImage (
+//         playerImage,
+//         0,
+//         0,
+//         playerImage.width / 4,
+//         playerImage.height,
+//         canvas.width / 2 - (playerImage.width / 4) / 2,
+//         canvas.height / 2 - playerImage.height / 2,
+//         playerImage.width / 4,
+//         playerImage.height
+//     )
+// }
+
+// 3. Making the character move upon pressing keys
+// class
+
+
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - (192 / 4) / 2,
+        y: canvas.height / 2 - 68 / 2
+    },
+    image: playerDownImage,
+    frames: {
+        max: 4
+    },
+    sprites: {
+        down: playerDownImage,
+        up: playerUpImage,
+        left: playerLeftImage,
+        right: playerRightImage
+    }
+})
+
+const background = new Sprite({
+    position: {
+    x: offset.x,
+    y: offset.y
+    },
+    image: image
+})
+
+const foreground = new Sprite({
+    position: {
+    x: offset.x,
+    y: offset.y
+    },
+    image: foregroundImage
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+const movables = [background, ...boundaries, foreground]
+
+function rectangularCollision({rectangle1, rectangle2}) {
+    return (
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x && 
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+    )
+}
+
+// animation with loop
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+    boundaries.forEach(boundary => {
+        boundary.draw()
+    })
+    player.draw()
+    foreground.draw()
+
+    let moving = true
+    player.moving = false
+    if (keys.w.pressed && lastKey === 'w') {
+        player.moving = true
+        player.image = player.sprites.up
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: {
+                        ...boundary, 
+                        position: {
+                            x: boundary.position.x,
+                            y: boundary.position.y + 3
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+        if (moving)
+        movables.forEach((movable) => {
+            movable.position.y += 3
+        })
+    }
+    else if (keys.a.pressed && lastKey === 'a') {
+        player.moving = true
+        player.image = player.sprites.left
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: {
+                        ...boundary, 
+                        position: {
+                            x: boundary.position.x + 3,
+                            y: boundary.position.y
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+        if (moving)
+        movables.forEach((movable) => {
+            movable.position.x += 3
+        })
+    }
+    else if (keys.s.pressed && lastKey === 's') {
+        player.moving = true
+        player.image = player.sprites.down
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: {
+                        ...boundary, 
+                        position: {
+                            x: boundary.position.x,
+                            y: boundary.position.y - 3
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+        if (moving)
+        movables.forEach((movable) => {
+            movable.position.y -= 3
+        })
+    }
+    else if (keys.d.pressed && lastKey === 'd') {
+        player.moving = true
+        player.image = player.sprites.right
+        for (let i = 0; i < boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                rectangularCollision({
+                    rectangle1: player,
+                    rectangle2: {
+                        ...boundary, 
+                        position: {
+                            x: boundary.position.x - 3,
+                            y: boundary.position.y
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+        if (moving)
+        movables.forEach((movable) => {
+            movable.position.x -= 3
+        })
+    }
+}
+animate()
+
+// events
+let lastKey = ''
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = true
+            lastKey = 'w'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 's':
+            keys.s.pressed = true
+            lastKey = 's'
+            break
+        case 'd':
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+    }
+})
